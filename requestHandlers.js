2c53e99689d2bdfc70fd0957adf901576ca1638a
Takayuki Hirayama <takayuki.hirayama@peroli.jp>
2017-01-10 12:44:19 +0900

    remove old start()

--- requestHandlers.js
+++ requestHandlers.js
@@ -12,18 +12,6 @@ function start(response) {
         });
 }
 
-function _start() {
-    console.log("Request handler 'start' was called.");
-
-    function sleep(milliSeconds) {
-        var startTime = new Date().getTime();
-        while (new Date().getTime() < startTime + milliSeconds);
-    }
-
-    sleep(10000);
-    return "Hello Start";
-}
-
 function upload(response) {
     console.log("Request handler 'upload' was called.");
     response.writeHead(200, {"Content-Type": "text/plain"});
